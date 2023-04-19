0bb254f6ba540c350ab24a80efae995f63d1d4a7
James Duncombe <james.duncombe@consilienceventures.com>
2023-04-19 16:15:11 +0100

    Moves Transaction to LegacyTransaction.

--- lib/tt_eth/transactions/legacy_transaction.ex
+++ lib/tt_eth/transactions/legacy_transaction.ex
@@ -1,8 +1,11 @@
-defmodule TTEth.Transaction do
+defmodule TTEth.Transactions.LegacyTransaction do
   @moduledoc """
   Ported from [`Blockchain`](https://hex.pm/packages/blockchain).
   """
   alias TTEth.{BitHelper, Secp256k1}
+  alias TTEth.Behaviours.Transaction
+
+  @behaviour Transaction
 
   @type private_key :: <<_::256>>
 
@@ -16,6 +19,7 @@ defmodule TTEth.Transaction do
 
   @type t :: %__MODULE__{
           nonce: integer(),
+          chain_id: integer(),
           gas_price: integer(),
           gas_limit: integer(),
           to: address() | <<_::0>>,
@@ -28,13 +32,14 @@ defmodule TTEth.Transaction do
         }
 
   defstruct nonce: 0,
+            chain_id: 0,
             gas_price: 0,
             gas_limit: 0,
             to: <<>>,
             value: 0,
-            v: nil,
-            r: nil,
-            s: nil,
+            v: 0,
+            r: 0,
+            s: 0,
             init: <<>>,
             data: <<>>
 
@@ -42,22 +47,48 @@ defmodule TTEth.Transaction do
   @base_recovery_id 27
   @base_recovery_id_eip_155 35
 
+  @impl Transaction
+  def new("" <> to_address, abi_data, nonce, opts) when is_integer(nonce),
+    do: %__MODULE__{
+      chain_id: opts |> Keyword.get(:chain_id),
+      data: abi_data,
+      gas_limit: opts |> Keyword.get(:gas_limit, 500_000),
+      gas_price: opts |> Keyword.get(:gas_price, 1),
+      nonce: nonce,
+      to: to_address,
+      value: opts |> Keyword.get(:value, 0)
+    }
+
+  @impl Transaction
+  def build(%__MODULE__{} = trx, private_key),
+    do:
+      trx
+      |> sign_transaction(private_key)
+      |> serialize(_include_signature = true)
+      |> rlp_encode()
+
+  @doc """
+  Delegate to ExRLP to RLP encode values.
+  """
+  def rlp_encode(data),
+    do: data |> ExRLP.encode()
+
   @doc """
   Encodes a transaction such that it can be RLP-encoded.
   This is defined at L_T Eq.(14) in the Yellow Paper.
 
   ## Examples
 
-      iex> Transaction.serialize(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<1::160>>, value: 8, v: 27, r: 9, s: 10, data: "hi"})
+      iex> LegacyTransaction.serialize(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<1::160>>, value: 8, v: 27, r: 9, s: 10, data: "hi"})
       [<<5>>, <<6>>, <<7>>, <<1::160>>, <<8>>, "hi", <<27>>, <<9>>, <<10>>]
 
-      iex> Transaction.serialize(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 8, v: 27, r: 9, s: 10, init: <<1, 2, 3>>})
+      iex> LegacyTransaction.serialize(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 8, v: 27, r: 9, s: 10, init: <<1, 2, 3>>})
       [<<5>>, <<6>>, <<7>>, <<>>, <<8>>, <<1, 2, 3>>, <<27>>, <<9>>, <<10>>]
 
-      iex> Transaction.serialize(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 8, v: 27, r: 9, s: 10, init: <<1, 2, 3>>}, false)
+      iex> LegacyTransaction.serialize(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 8, v: 27, r: 9, s: 10, init: <<1, 2, 3>>}, false)
       [<<5>>, <<6>>, <<7>>, <<>>, <<8>>, <<1, 2, 3>>]
 
-      iex> Transaction.serialize(%Transaction{ data: "", gas_limit: 21000, gas_price: 20000000000, init: "", nonce: 9, r: 0, s: 0, to: "55555555555555555555", v: 1, value: 1000000000000000000 })
+      iex> LegacyTransaction.serialize(%LegacyTransaction{ data: "", gas_limit: 21000, gas_price: 20000000000, init: "", nonce: 9, r: 0, s: 0, to: "55555555555555555555", v: 1, value: 1000000000000000000 })
       ["\t", <<4, 168, 23, 200, 0>>, "R\b", "55555555555555555555", <<13, 224, 182, 179, 167, 100, 0, 0>>, "", <<1>>, "", ""]
 
   """
@@ -91,12 +122,12 @@ defmodule TTEth.Transaction do
 
   ## Examples
 
-      iex> Transaction.sign_hash(<<2::256>>, <<1::256>>)
+      iex> LegacyTransaction.sign_hash(<<2::256>>, <<1::256>>)
       {28,
       38938543279057362855969661240129897219713373336787331739561340553100525404231,
       23772455091703794797226342343520955590158385983376086035257995824653222457926}
 
-      iex> Transaction.sign_hash(<<5::256>>, <<1::256>>)
+      iex> LegacyTransaction.sign_hash(<<5::256>>, <<1::256>>)
       {27,
       74927840775756275467012999236208995857356645681540064312847180029125478834483,
       56037731387691402801139111075060162264934372456622294904359821823785637523849}
@@ -104,7 +135,7 @@ defmodule TTEth.Transaction do
       iex> data = "ec098504a817c800825208943535353535353535353535353535353535353535880de0b6b3a764000080018080" |> TTEth.BitHelper.from_hex
       iex> hash = data |> TTEth.keccak()
       iex> private_key = "4646464646464646464646464646464646464646464646464646464646464646" |> TTEth.BitHelper.from_hex
-      iex> Transaction.sign_hash(hash, private_key, 1)
+      iex> LegacyTransaction.sign_hash(hash, private_key, 1)
       { 37, 18515461264373351373200002665853028612451056578545711640558177340181847433846, 46948507304638947509940763649030358759909902576025900602547168820602576006531 }
 
   """
@@ -134,13 +165,13 @@ defmodule TTEth.Transaction do
 
   ## Examples
 
-      iex> Transaction.transaction_hash(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 5, init: <<1>>})
+      iex> LegacyTransaction.transaction_hash(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 5, init: <<1>>})
       <<127, 113, 209, 76, 19, 196, 2, 206, 19, 198, 240, 99, 184, 62, 8, 95, 9, 122, 135, 142, 51, 22, 61, 97, 70, 206, 206, 39, 121, 54, 83, 27>>
 
-      iex> Transaction.transaction_hash(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<1>>, value: 5, data: <<1>>})
+      iex> LegacyTransaction.transaction_hash(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<1>>, value: 5, data: <<1>>})
       <<225, 195, 128, 181, 3, 211, 32, 231, 34, 10, 166, 198, 153, 71, 210, 118, 51, 117, 22, 242, 87, 212, 229, 37, 71, 226, 150, 160, 50, 203, 127, 180>>
 
-      iex> Transaction.transaction_hash(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<1>>, value: 5, data: <<1>>}, 1)
+      iex> LegacyTransaction.transaction_hash(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<1>>, value: 5, data: <<1>>}, 1)
       <<132, 79, 28, 4, 212, 58, 235, 38, 66, 211, 167, 102, 36, 58, 229, 88, 238, 251, 153, 23, 121, 163, 212, 64, 83, 111, 200, 206, 54, 43, 112, 53>>
 
   """
@@ -151,7 +182,7 @@ defmodule TTEth.Transaction do
       |> serialize(false)
       # See EIP-155
       |> Kernel.++(if chain_id, do: [chain_id |> :binary.encode_unsigned(), <<>>, <<>>], else: [])
-      |> ExRLP.encode()
+      |> rlp_encode()
       |> TTEth.keccak()
 
   @doc """
@@ -161,11 +192,11 @@ defmodule TTEth.Transaction do
 
   ## Examples
 
-      iex> Transaction.sign_transaction(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 5, init: <<1>>}, <<1::256>>)
-      %Transaction{data: <<>>, gas_limit: 7, gas_price: 6, init: <<1>>, nonce: 5, r: 97037709922803580267279977200525583527127616719646548867384185721164615918250, s: 31446571475787755537574189222065166628755695553801403547291726929250860527755, to: "", v: 27, value: 5}
+      iex> LegacyTransaction.sign_transaction(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 5, init: <<1>>}, <<1::256>>)
+      %LegacyTransaction{data: <<>>, gas_limit: 7, gas_price: 6, init: <<1>>, nonce: 5, r: 97037709922803580267279977200525583527127616719646548867384185721164615918250, s: 31446571475787755537574189222065166628755695553801403547291726929250860527755, to: "", v: 27, value: 5}
 
-      iex> Transaction.sign_transaction(%Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 5, init: <<1>>}, <<1::256>>, 1)
-      %Transaction{data: <<>>, gas_limit: 7, gas_price: 6, init: <<1>>, nonce: 5, r: 25739987953128435966549144317523422635562973654702886626580606913510283002553, s: 41423569377768420285000144846773344478964141018753766296386430811329935846420, to: "", v: 38, value: 5}
+      iex> LegacyTransaction.sign_transaction(%LegacyTransaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<>>, value: 5, init: <<1>>}, <<1::256>>, 1)
+      %LegacyTransaction{data: <<>>, gas_limit: 7, gas_price: 6, init: <<1>>, nonce: 5, r: 25739987953128435966549144317523422635562973654702886626580606913510283002553, s: 41423569377768420285000144846773344478964141018753766296386430811329935846420, to: "", v: 38, value: 5}
 
   """
   @spec sign_transaction(__MODULE__.t(), private_key, integer() | nil) :: __MODULE__.t()
--- test/tt_eth/transaction_test.exs
+++
@@ -1,5 +0,0 @@
-defmodule TTEth.TransactionTest do
-  use TTEth.Case
-  alias TTEth.Transaction
-  doctest Transaction, import: true
-end
