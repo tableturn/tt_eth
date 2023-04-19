d653c5a4937983be5bbd53f8c3feb8d65cac3f5b
James Duncombe <james.duncombe@consilienceventures.com>
2023-04-19 16:16:14 +0100

    Adds EIP1559 Transactions.

---
+++ lib/tt_eth/transactions/eip1559_transaction.ex
@@ -0,0 +1,147 @@
+defmodule TTEth.Transactions.EIP1559Transaction do
+  @moduledoc """
+  Represents an EIP1559 transaction.
+
+  SEE: https://eips.ethereum.org/EIPS/eip-1559
+  """
+  alias TTEth.{BitHelper, Secp256k1}
+  alias TTEth.Behaviours.Transaction
+  import BitHelper, only: [encode_unsigned: 1]
+
+  @behaviour Transaction
+
+  defstruct type: 2,
+            nonce: 0,
+            chain_id: 0,
+            gas_limit: 0,
+            max_fee_per_gas: 0,
+            max_priority_fee_per_gas: 0,
+            to: <<>>,
+            value: 0,
+            access_list: [],
+            y_parity: 0,
+            r: 0,
+            s: 0,
+            init: <<>>,
+            data: <<>>
+
+  # Base recovery V minimum.
+  @base_recovery_id 27
+
+  # The transaction envelope version for an EIP-1559 transaction.
+  # SEE: https://eips.ethereum.org/EIPS/eip-2718
+  # SEE: https://eips.ethereum.org/EIPS/eip-1559
+  @transaction_type 2
+
+  @doc """
+  Creates a new type 2 transaction that can be signed.
+  """
+  @impl Transaction
+  def new("" <> to_address, abi_data, nonce, opts) when is_integer(nonce),
+    do: %__MODULE__{
+      type: @transaction_type,
+      chain_id: opts[:chain_id],
+      data: abi_data,
+      gas_limit: opts |> Keyword.get(:gas_limit, 500_000),
+      max_fee_per_gas: opts |> Keyword.get(:max_fee_per_gas, 500_000),
+      max_priority_fee_per_gas: opts |> Keyword.get(:max_priority_fee_per_gas, 500_000),
+      nonce: nonce,
+      to: to_address,
+      value: opts |> Keyword.get(:value, 0)
+    }
+
+  @doc """
+  Take a transaction `trx` and build it into a signed transaction.
+
+  This will return a binary which can then be base16 encoded etc.
+  """
+  @impl Transaction
+  def build(%__MODULE__{} = trx, private_key),
+    do:
+      trx
+      |> sign_transaction(private_key)
+      |> serialize(_include_signature = true)
+      |> rlp_encode()
+      |> put_transaction_envelope(trx)
+
+  @doc """
+  Delegate to ExRLP to RLP encode values.
+  """
+  def rlp_encode(data),
+    do: data |> ExRLP.encode()
+
+  @doc """
+  Encodes a transaction such that it can be RLP-encoded.
+  """
+  def serialize(%__MODULE__{} = trx, include_vrs \\ true),
+    do:
+      [
+        trx.chain_id |> encode_unsigned(),
+        trx.nonce |> encode_unsigned(),
+        trx.max_priority_fee_per_gas |> encode_unsigned(),
+        trx.max_fee_per_gas |> encode_unsigned(),
+        trx.gas_limit |> encode_unsigned(),
+        trx.to,
+        trx.value |> encode_unsigned(),
+        if(trx.to == <<>>, do: trx.init, else: trx.data),
+        trx.access_list
+      ]
+      |> maybe_add_yrs(trx, include_vrs)
+
+  @doc """
+  Returns a ECDSA signature (v,r,s) for a given hashed value.
+  """
+  def sign_hash(hash, private_key) do
+    {:ok, {<<r::size(256), s::size(256)>>, v}} = Secp256k1.ecdsa_sign_compact(hash, private_key)
+
+    {v |> v_to_parity(), r, s}
+  end
+
+  @doc """
+  Returns a hash of a given transaction.
+  """
+  def transaction_hash(%__MODULE__{} = trx),
+    do:
+      trx
+      |> serialize(_include_signature = false)
+      |> rlp_encode()
+      |> put_transaction_envelope(trx)
+      |> TTEth.keccak()
+
+  @doc """
+  Takes a given transaction and returns a version signed with the given private key.
+  """
+  def sign_transaction(%__MODULE__{} = trx, private_key) when is_binary(private_key) do
+    {y_parity, r, s} =
+      trx
+      |> transaction_hash()
+      |> sign_hash(private_key)
+
+    %{trx | y_parity: y_parity, r: r, s: s}
+  end
+
+  @doc """
+  Wraps the RLP encoded transaction in a transaction envelope.
+  SEE: https://eips.ethereum.org/EIPS/eip-2718
+  """
+  def put_transaction_envelope(encoded, %__MODULE__{} = trx) when is_binary(encoded),
+    do: <<trx.type>> <> encoded
+
+  ## Private.
+
+  # Converts the `v` value to a parity value.
+  defp v_to_parity(@base_recovery_id), do: 0
+  defp v_to_parity(_), do: 1
+
+  # Optionally add the YRS values.
+  defp maybe_add_yrs(base, %__MODULE__{} = trx, _include_vrs = true),
+    do:
+      base ++
+        [
+          trx.y_parity |> encode_unsigned(),
+          trx.r |> encode_unsigned(),
+          trx.s |> encode_unsigned()
+        ]
+
+  defp maybe_add_yrs(base, %__MODULE__{}, _dont_include_vrs), do: base
+end
---
+++ test/tt_eth/transactions/eip1559_transaction_test.exs
@@ -0,0 +1,86 @@
+defmodule TTEth.Transactions.EIP1559TransactionTest do
+  use TTEth.Case
+  alias TTEth.Transactions.EIP1559Transaction
+
+  # Polygon Mumbai.
+  @chain_id 80001
+
+  @private_key_human "0x62aa6ec41b56439d2c5df352c45a00389cef262b3761e13c6481e35ab027d262"
+  @private_key @private_key_human |> TTEth.Type.PrivateKey.from_human!()
+  @to_address_human "0x38f153fdd399ff2cf64704c6a4b16d3fd9ddcd69"
+  @to_address @to_address_human |> TTEth.Type.Address.from_human!()
+  # transfer(address,uint256)
+  @tx_data_human "a9059cbb00000000000000000000000038f153fdd399ff2cf64704c6a4b16d3fd9ddcd69000000000000000000000000000000000000000000000000000000000000000a"
+  @tx_data @tx_data_human |> Base.decode16!(case: :lower)
+
+  # Expected valid transaction data.
+  @valid_transaction_data "0x02f8b2830138810184b2d05e0184b2d05e008252089438f153fdd399ff2cf6" <>
+                            "4704c6a4b16d3fd9ddcd6980b844a9059cbb000000000000000000000000" <>
+                            "38f153fdd399ff2cf64704c6a4b16d3fd9ddcd6900000000000000000000" <>
+                            "0000000000000000000000000000000000000000000ac001a027995f5230" <>
+                            "24701f3eb15f1a664651f127c09a70aacbc334dbe36c2fb9b87c4ea03572" <>
+                            "c5dfa49a14fce646eb8de6a4f3d6e2e25dacbb7a0dca8891f0f3e6cb801f"
+
+  @nonce 1
+
+  describe "new/4" do
+    test "returns an EIP1559 transaction struct" do
+      @to_address
+      |> EIP1559Transaction.new(
+        @tx_data,
+        @nonce,
+        _opts = [chain_id: @chain_id]
+      )
+      |> assert_match(%EIP1559Transaction{
+        type: 2,
+        nonce: @nonce,
+        chain_id: @chain_id,
+        gas_limit: 500_000,
+        max_fee_per_gas: 500_000,
+        max_priority_fee_per_gas: 500_000,
+        init: <<>>,
+        data: @tx_data,
+        to: @to_address,
+        access_list: [],
+        y_parity: 0,
+        r: 0,
+        s: 0
+      })
+    end
+  end
+
+  describe "build/2" do
+    setup :build_trx
+
+    test "builds a signed transaction", %{trx: trx} do
+      trx
+      |> EIP1559Transaction.build(@private_key)
+      |> encode_and_pad()
+      |> assert_match(@valid_transaction_data)
+    end
+  end
+
+  ## Private.
+
+  defp build_trx(_),
+    do: %{
+      trx: %EIP1559Transaction{
+        to: @to_address,
+        data: @tx_data,
+        chain_id: @chain_id,
+        nonce: @nonce,
+        gas_limit: 21000,
+        max_fee_per_gas: 3_000_000_000,
+        max_priority_fee_per_gas: 3_000_000_001,
+        value: 0
+      }
+    }
+
+  ## Helpers.
+
+  defp encode_and_pad(bin),
+    do:
+      bin
+      |> Base.encode16(case: :lower)
+      |> (&("0x" <> &1)).()
+end
