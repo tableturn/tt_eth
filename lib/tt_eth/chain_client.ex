d210455793b3f20794edc690732ce8bbda396981
James Duncombe <james.duncombe@consilienceventures.com>
2023-04-19 16:16:44 +0100

    Adds transaction_module support to swap Transaction types.

--- lib/tt_eth/chain_client.ex
+++ lib/tt_eth/chain_client.ex
@@ -1,58 +1,64 @@
 defmodule TTEth.ChainClient do
   @moduledoc """
   A wrapper around `Ethereumex` providing the basics for various chain actions.
+
+  This is agnostic to the transaction version/type.
   """
   alias TTEth.Behaviours.ChainClient
-  alias TTEth.Transaction
+  alias Ethereumex.HttpClient
+  import TTEth, only: [transaction_module: 0]
 
   @behaviour ChainClient
+
+  ## ChainClient specific behaviour.
 
   @impl ChainClient
   def eth_call(contract_address, encoded_args, opts \\ []),
     do:
-      %{data: "0x" <> encoded_args, to: contract_address}
-      |> Ethereumex.HttpClient.eth_call(opts |> Keyword.get(:block, "latest"), opts)
+      %{data: encoded_args |> prepend_hex(), to: contract_address}
+      |> HttpClient.eth_call(opts |> Keyword.get(:block, "latest"), opts)
 
   @impl ChainClient
   def eth_send_raw_transaction(tx_data, opts \\ []),
-    do: tx_data |> Ethereumex.HttpClient.eth_send_raw_transaction(opts)
+    do: tx_data |> HttpClient.eth_send_raw_transaction(opts)
 
+  # Delegate to the transaction module to serialize and sign the transaction.
   @impl ChainClient
-  def build_tx_data(to, abi_data, private_key, nonce, opts \\ []),
-    do:
-      "0x" <>
-        (%Transaction{
-           data: abi_data,
-           gas_limit: opts |> Keyword.get(:gas_limit, 500_000),
-           gas_price: opts |> Keyword.get(:gas_price, 1),
-           nonce: nonce,
-           to: to,
-           value: opts |> Keyword.get(:value, 0),
-           r: 0,
-           s: 0,
-           v: 0
-         }
-         |> Transaction.sign_transaction(private_key, opts[:chain_id])
-         |> Transaction.serialize()
-         |> ExRLP.encode()
-         |> Base.encode16(case: :lower))
-
-  def eth_get_transaction_receipt(tx_hash, opts \\ []),
-    do: tx_hash |> Ethereumex.HttpClient.eth_get_transaction_receipt(opts)
+  def build_tx_data("" <> to_address, abi_data, private_key, nonce, opts \\ [])
+      when is_integer(nonce),
+      do:
+        to_address
+        |> transaction_module().new(abi_data, nonce, opts)
+        |> transaction_module().build(private_key)
+        |> Base.encode16(case: :lower)
+        |> prepend_hex()
 
   @impl ChainClient
   def eth_get_transaction_count(address, block \\ "latest", opts \\ []),
-    do: address |> Ethereumex.HttpClient.eth_get_transaction_count(block, opts)
+    do: address |> HttpClient.eth_get_transaction_count(block, opts)
 
   @impl ChainClient
   def eth_new_filter(params, opts \\ []),
-    do: params |> Ethereumex.HttpClient.eth_new_filter(opts)
+    do: params |> HttpClient.eth_new_filter(opts)
 
   @impl ChainClient
   def eth_get_filter_logs(filter_id, opts \\ []),
-    do: filter_id |> Ethereumex.HttpClient.eth_get_filter_logs(opts)
+    do: filter_id |> HttpClient.eth_get_filter_logs(opts)
 
   @impl ChainClient
   def eth_get_filter_changes(filter_id, opts \\ []),
-    do: filter_id |> Ethereumex.HttpClient.eth_get_filter_changes(opts)
+    do: filter_id |> HttpClient.eth_get_filter_changes(opts)
+
+  ## Helpers outside of the ChainClient behaviour.
+
+  def eth_estimate_gas(tx_hash, opts \\ []),
+    do: tx_hash |> HttpClient.eth_estimate_gas(opts)
+
+  def eth_get_transaction_receipt(tx_hash, opts \\ []),
+    do: tx_hash |> HttpClient.eth_get_transaction_receipt(opts)
+
+  ## Private.
+
+  defp prepend_hex(data),
+    do: "0x" <> data
 end
