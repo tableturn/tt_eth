f0f60520b55228ba955b7211f88cf744a9bed4d2
James Duncombe <james.duncombe@consilienceventures.com>
2023-04-19 17:24:46 +0100

    Fixes test config.

--- config/config.exs
+++ config/config.exs
@@ -9,4 +9,5 @@ config :tt_eth,
     primary: "0xf90f7e5e801d8820666a1c941e518eae78601e1a4c4305bbe18217dd7bc6d030",
     secondary: "0xe2187dc017e880dded10c403f7c0d397afb11736ac027c1202e318b0dd345086",
     ternary: "0xfa015243f2e6d8694ab037a7987dc73b1630fc8cb1ce82860344684c15d24026"
-  ]
+  ],
+  transaction_module: TTEth.Transactions.LegacyTransaction
